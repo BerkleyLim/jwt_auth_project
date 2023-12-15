b74e98fbdd52584ebabb14d753440eba6babd225
berkleylim <berkleylim16@gmail.com>
2023-12-15 13:57:30 +0900

    chron: front-end 셋팅 - React 설치 - typescript 설정 - tailwind css 설정

---
+++ jwt-front/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="py-2 px-4 rounded-lg shadow-md text-white bg-blue-500">click me!!</button>
+    </div>
+  );
+}
+
+export default App;
